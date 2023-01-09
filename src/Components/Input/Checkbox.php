c85f67c2426910b1b378973aab399b47722129fd
eugenpasca <eugenpasca@users.noreply.github.com>
2023-01-09 22:45:44 +0000

    Fix styling

--- src/Components/Input/Checkbox.php
+++ src/Components/Input/Checkbox.php
@@ -23,8 +23,8 @@ class Checkbox extends Component
     /**
      * Create the component instance.
      *
-     * @param bool $on
-     * @param bool $disabled
+     * @param  bool  $on
+     * @param  bool  $disabled
      */
     public function __construct(bool $on = false, bool $disabled = false)
     {
