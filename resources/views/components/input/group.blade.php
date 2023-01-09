54f088150764d0d20d8d05aed9e0f219e5bb5915
Eugen Pasca <pasca.eugen@gmail.com>
2023-01-09 23:43:52 +0200

    added Input group

---
+++ resources/views/components/input/group.blade.php
@@ -0,0 +1,36 @@
+<div  class="form-group @if(count($errors)) error @endif">
+    <label for="{{ $for }}"
+          >
+        <span class="block">{{ $labelPrefix ?? null }}</span>
+
+        <span class="block">
+            {{ $label }}
+
+            @if ($required)
+                <sup class="text-xs text-red-400">&#42;</sup>
+            @endif
+        </span>
+    </label>
+
+    <div class="relative mt-1">
+        {{ $slot }}
+    </div>
+
+    @if ($instructions)
+        <p class="mt-2 text-sm text-gray-500">{{ $instructions }}</p>
+    @endif
+
+    @if (count($errors))
+        <div class="space-y-1">
+            @foreach ($errors as $error)
+                @if (is_array($error))
+                    @foreach ($error as $text)
+                        <p class="text-sm text-red-600">{{ $text }}</p>
+                    @endforeach
+                @else
+                    <p class="text-sm text-red-600">{{ $error }}</p>
+                @endif
+            @endforeach
+        </div>
+    @endif
+</div>
---
+++ src/Components/Input/Group.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Hehecoding\LaragenUiKit\Components\Input;
+
+use Illuminate\View\Component;
+
+class Group extends Component
+{
+    /**
+     * The label for the group.
+     *
+     * @var string
+     */
+    public string $label;
+
+    /**
+     * Specify what input this label is bound to.
+     *
+     * @var string
+     */
+    public string $for;
+
+    /**
+     * An array of validation errors.
+     *
+     * @var array
+     */
+    public array $errors = [];
+
+    /**
+     * Any instructions which should be rendered.
+     *
+     * @var null|string
+     */
+    public ?string $instructions;
+
+    /**
+     * Whether this input group is required.
+     *
+     * @var bool
+     */
+    public bool $required = false;
+
+
+    /**
+     * Create the component instance.
+     *
+     * @param string $label
+     * @param string $for
+     * @param string|null $instructions
+     * @param bool $required
+     * @param array $errors
+     */
+    public function __construct(string $label, string $for, ?string $instructions = null, bool $required = false, array $errors = [])
+    {
+        $this->label = $label;
+        $this->for = $for;
+        $this->instructions = $instructions;
+        $this->required = $required;
+        $this->errors = $errors;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @noinspection PhpFullyQualifiedNameUsageInspection
+     */
+    public function render()
+    {
+        return view('laragen::components.input.group');
+    }
+}
