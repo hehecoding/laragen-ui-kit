c498978b19135754330dd19439526a410f602918
Eugen Pasca <pasca.eugen@gmail.com>
2023-01-10 00:45:30 +0200

    added Checkbox component

---
+++ src/Components/Input/Checkbox.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Hehecoding\LaragenUiKit\Components\Input;
+
+use Illuminate\View\Component;
+
+class Checkbox extends Component
+{
+    /**
+     * Whether the toggle should be in an on state.
+     *
+     * @var bool
+     */
+    public bool $on = false;
+
+    /**
+     * Whether the toggle should be disabled.
+     *
+     * @var bool
+     */
+    public bool $disabled = false;
+
+    /**
+     * Create the component instance.
+     *
+     * @param bool $on
+     * @param bool $disabled
+     */
+    public function __construct(bool $on = false, bool $disabled = false)
+    {
+        $this->on = $on;
+        $this->disabled = $disabled;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('laragen::components.input.checkbox');
+    }
+}
