724cf40bf5553c5a3d0fcfc11ed9f1fb4d1036d0
Vladulescu <ale.vladulescu@gmail.com>
2023-04-20 17:54:51 +0300

    toast component styling

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -25,7 +25,8 @@
             add(event.detail);
         });
     }"
-    class="">
+    class="w-80 flex flex-col items-end space-y-4 fixed bottom-4 right-4"
+    xmlns:x-laragen="http://www.w3.org/1999/html">
     <template x-for="notice of notices" :key="notice.id">
         <div
             x-show="visible.includes(notice)"
@@ -35,16 +36,28 @@
             x-transition:leave="transition ease-out duration-500"
             x-transition:leave-start="transform translate-x-0 opacity-100"
             x-transition:leave-end="transform translate-x-full opacity-0"
-            @click="remove(notice.id)"
-            class="ml-auto rounded mb-4 w-56 h-16 flex items-center justify-center text-white shadow-lg font-bold text-lg cursor-pointer px-4 py-2"
-            :class="{
-                'bg-green-500': notice.type === 'success',
-                'bg-blue-500': notice.type === 'info',
-                'bg-orange-500': notice.type === 'warning',
-                'bg-red-500': notice.type === 'error'
-             }"
-            style="pointer-events:auto"
-            x-text="notice.text">
+            class="mb-2 pointer-events-auto w-full overflow-hidden rounded bg-white shadow ring-1 ring-black ring-opacity-5"
+        >
+            <div class="p-4">
+                <div class="flex items-start">
+                    <div class="flex-shrink-0">
+                        <i class="h-6 w-6" :class="{
+                'fa-light fa-circle-check text-green-400': notice.type === 'success',
+                'fa-light fa-circle-info text-blue-400': notice.type === 'info',
+                'fa-light fa-circle-exclamation text-orange-400': notice.type === 'warning',
+                'fa-light fa-circle-xmark text-red-400': notice.type === 'error'
+             }"></i>
+                    </div>
+                    <div class="ml-3 w-0 flex-1">
+                        <p class="text-sm font-medium text-gray-600" x-text="notice.text"></p>
+                    </div>
+                    <div class="ml-4 flex flex-shrink-0">
+                        <x-laragen::button @click="remove(notice.id)" icon="fa-light fa-times" variant="text"
+                                           type="button" color="gray" size="icon">
+                        </x-laragen::button>
+                    </div>
+                </div>
+            </div>
         </div>
     </template>
 </div>
